f020b64c55bd34d58485ca7cd7324b48d3a53f65
MysticalX99 <55935446+MysticalX99@users.noreply.github.com>
2019-10-10 19:31:48 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// my C++ starter code
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+
+int main() {
+  cin.tie(0);
+  cin.sync_with_stdio(0);
+
+  return 0;
+}
+
